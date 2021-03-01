7156f7054310f36a94d89dadc4f3173d3100989c
Janik Andrejkovic <jandrejkovic@gmx.net>
2021-03-01 14:01:47 +0100

    improvements

--- FF_derivation/AutomaticSubmission/fix.sh
+++ FF_derivation/AutomaticSubmission/fix.sh
@@ -3,16 +3,28 @@
 chan='mt'
 channel='kMU'
 
-# for year in 2016 #2017 2018
-# do
-    
-#     cd ${year}/${chan}/CMSSW_8_0_25/src
-#     ./convert_inputs
-#     python cpTDHaftPublic.py --destination /ceph/jandrej/auto-puppi-fakefactors/16_4_${year}_v7/ --channel ${channel} --doNjetBinning 0
-#     python producePublicFakeFactors.py --input /ceph/jandrej/auto-puppi-fakefactors/16_4_${year}_v7/ --channel ${channel} --njetbinning 0
-#     cd -
-# done 
+# chan='et'
+# channel='kEL'
 
+# chan='tt'
+# channel='kTAU'
+
+echo "This script needs manual adjustments"
+exit
+
+
+for year in 2016 2017 2018
+do
+
+    cd ${year}/${chan}/CMSSW_8_0_25/src
+    ./convert_inputs
+    python cpTDHaftPublic.py --destination /ceph/jandrej/auto-fakefactors/16_4_${year}_FF_LQ_v1p1/ --channel ${channel} --doNjetBinning 0
+    python producePublicFakeFactors.py --input /ceph/jandrej/auto-fakefactors/16_4_${year}_FF_LQ_v1p1/ --channel ${channel} --njetbinning 0
+    cd -
+done 
+
+
+exit
 for year in 2016 2017 2018
 do
     for CHAN in et mt tt 
--- FF_derivation/AutomaticSubmission/script.sh
+++ FF_derivation/AutomaticSubmission/script.sh
@@ -10,7 +10,10 @@ mkdir ${ERA}/${CHANNEL} -p
 cd ${ERA}/${CHANNEL}
 cmsrel CMSSW_8_0_25
 cd CMSSW_8_0_25/src/; cmsenv
-git clone https://github.com/jandrejk/FakeFactor.git -b FF_DeepTau_MSSM_${ERA}_v0 .
+
+git clone https://github.com/jandrejk/FakeFactor.git -b FF_DeepTau_MSSM_${ERA}_v0 . #use of MSSM HTT analysis
+# git clone https://github.com/jandrejk/FakeFactor.git -b FF_DeepTau_LQ_${ERA} . #use of LQ analysis
+# git clone https://github.com/jandrejk/FakeFactor.git -b FF_DeepTau_${ERA}_v15_v7 . #use of SM HTT
 git clone https://github.com/CMS-HTT/Jet2TauFakes.git HTTutilities/Jet2TauFakes
 cd HTTutilities/Jet2TauFakes
 git checkout v0.2.1
@@ -23,12 +26,12 @@ mkdir /ceph/jandrej/auto-fakefactors/${PASS}_${FAIL}_${ERA}_${EXT}/${CHANNEL}/Im
 if [ "$CHANNEL" == "et" ];  then f_chan="kEL"; fi
 if [ "$CHANNEL" == "mt" ];  then f_chan="kMU"; fi
 if [ "$CHANNEL" == "tt" ]; then f_chan="kTAU"; fi
-sed -i 's#selCHAN      kMU#selCHAN      '${f_chan}'#g' ViennaTool/Settings.h
-sed -i 's#selCHAN      kEL#selCHAN      '${f_chan}'#g' ViennaTool/Settings.h
-sed -i 's#selCHAN      kTAU#selCHAN      '${f_chan}'#g' ViennaTool/Settings.h
-sed -i 's#CHAN    = kMU#CHAN     = '${f_chan}'#g' ViennaTool/Settings.h
-sed -i 's#CHAN    = kEL#CHAN     = '${f_chan}'#g' ViennaTool/Settings.h
-sed -i 's#CHAN    = kTAU#CHAN    = '${f_chan}'#g' ViennaTool/Settings.h
+sed -i 's#selCHAN\s*kMU#selCHAN      '${f_chan}'#g' ViennaTool/Settings.h
+sed -i 's#selCHAN\s*kEL#selCHAN      '${f_chan}'#g' ViennaTool/Settings.h
+sed -i 's#selCHAN\s*kTAU#selCHAN      '${f_chan}'#g' ViennaTool/Settings.h
+sed -i 's#CHAN\s*=\s*kMU#CHAN     = '${f_chan}'#g' ViennaTool/Settings.h
+sed -i 's#CHAN\s*=\s*kEL#CHAN     = '${f_chan}'#g' ViennaTool/Settings.h
+sed -i 's#CHAN\s*=\s*kTAU#CHAN     = '${f_chan}'#g' ViennaTool/Settings.h
 sed -i 's#wpTightFulfill.*#wpTightFulfill = '${PASS}';#g' ViennaTool/Settings.h
 sed -i 's#wpLooseFulfill.*#wpLooseFulfill  = '${FAIL}';#g' ViennaTool/Settings.h
 sed -i 's#wpLooseFail.*#wpLooseFail    = '${PASS}';#g' ViennaTool/Settings.h
@@ -39,5 +42,5 @@ sed -i 's#analysis.*#analysis ="'${PASS}'_'${FAIL}'_'${ERA}'_'${EXT}'";#g' Vienn
 
 make -B
 cd ViennaTool
-cp BuildStructure_template.sh BuildStructure.sh
-echo -ne '\n' | ./steerAll.sh 1
+
+echo -ne '\n' | ./steerAll.sh 1 KIT 0
