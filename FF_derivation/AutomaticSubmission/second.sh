64abf6f79ed87669961c17af6980fa44cc62ddf1
janik.andrejkovic <janik.andrejkovic@clip-login-0.cbe.vbc.ac.at>
2022-10-27 15:42:44 +0200

    2018 mt needs more memory and walltime

--- FF_derivation/AutomaticSubmission/second.sh
+++ FF_derivation/AutomaticSubmission/second.sh
@@ -4,12 +4,19 @@ EXT='FF_LQ_v2p2'
 VER='v2p2'
 USER='janik.andrejkovic'
 
-for ERA in 2016 2017 2018 
+#switch back pre-sel and put 3 hours wall-time
+for ERA in 2016 2018 #2016 2017 2018 
 do
-    for CHANNEL in et mt tt
+    for CHANNEL in "mt" #"et" "mt" "tt"
     do
-    #    sh first.sh $ERA $CHANNEL $WP_pass $WP_fail $EXT $VER $USER &
-       submit "sh first.sh $ERA $CHANNEL $WP_pass $WP_fail $EXT $VER $USER" --walltime 02:00:00 --title "FF_$ERA\_$CHANNEL\_$WP_pass\_$WP_fail\_$EXT"
+        if [[ ("$CHANNEL" == "mt") && ($ERA -eq 2018) ]]
+        then
+            # 2018 mt needs more walltime and memory
+            submit "sh first.sh $ERA $CHANNEL $WP_pass $WP_fail $EXT $VER $USER" --walltime 03:00:00 --memory 8 --title "FF_$ERA\_$CHANNEL\_$WP_pass\_$WP_fail\_$EXT"
+        else
+            submit "sh first.sh $ERA $CHANNEL $WP_pass $WP_fail $EXT $VER $USER" --walltime 02:00:00 --title "FF_$ERA\_$CHANNEL\_$WP_pass\_$WP_fail\_$EXT"
+
+        fi
     done
 done
 # wait
